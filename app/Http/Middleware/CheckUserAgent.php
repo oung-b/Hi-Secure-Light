beca6bf5359f0860b8609a16ec339b02d579adf0
전웅배 <dndqo49>
2025-06-17 17:22:31 +0900

    로그인 세션을 redis에 저장하게 수정

--- app/Http/Middleware/CheckUserAgent.php
+++ app/Http/Middleware/CheckUserAgent.php
@@ -23,34 +23,23 @@ class CheckUserAgent
         $currentIpAddress = $request->ip();
         $currentUserAgent = $request->header('User-Agent');
 
-        // 쿠키에서 세션 ID 가져오기
-        $sessionId = $request->cookie('hi_secure_session');
+        // Redis 세션에서 데이터 가져오기 (세션이 Redis에 저장되므로)
+        $storedIpAddress = session('ip_address');
+        $storedUserAgent = session('user_agent');
 
-        if ($sessionId) {
-            $sessionFilePath = storage_path("framework/sessions/{$sessionId}");
-        
-            if (file_exists($sessionFilePath)) {
-                $sessionData = file_get_contents($sessionFilePath); // 세션 파일 내용 읽기
-                $payload = unserialize($sessionData); // 세션 데이터 복호화
-        
-                $storedIpAddress = $payload['ip_address'] ?? 'N/A';
-                $storedUserAgent = $payload['user_agent'] ?? 'N/A';
-        
-                // User-Agent 값이 다르면 로그아웃 처리
-                if (($currentIpAddress !== $storedIpAddress) || ($storedUserAgent !== $currentUserAgent)) {
-                    Log::warning('User-Agent mismatch!');
-        
-                    //Auth::logout(); // 로그아웃 처리
-                    //session()->invalidate(); // 현재 세션 무효화
-                    //session()->regenerateToken(); // CSRF 토큰 재생성
-        
-                    abort(403, '비정상적인 접근이 감지되었습니다.');
-                }
-            } else {
-                Log::error("Session file not found: {$sessionFilePath}");
+        if ($storedIpAddress && $storedUserAgent) {
+            // User-Agent 값이 다르면 로그아웃 처리
+            if (($currentIpAddress !== $storedIpAddress) || ($storedUserAgent !== $currentUserAgent)) {
+                Log::warning('User-Agent mismatch!');
+
+                //Auth::logout(); // 로그아웃 처리
+                //session()->invalidate(); // 현재 세션 무효화
+                //session()->regenerateToken(); // CSRF 토큰 재생성
+
+                abort(403, '비정상적인 접근이 감지되었습니다.');
             }
         } else {
-            Log::error("Session not found");
+            Log::info('Session data not found');
         }
 
         return $next($request);
