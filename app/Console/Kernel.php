9e65c15116d31ac0914d09608de8a20da2189a06
전웅배 <dndqo49>
2025-06-24 09:58:11 +0900

    schedule 1분마다

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('record:histories')->everyFiveMinutes();
+        $schedule->command('record:histories')->everyMinute();
     }
 
     /**
